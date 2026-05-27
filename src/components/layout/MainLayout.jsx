9b01072e0b3c77afb1eca5feade3e5328218e4ba
VISHNUVK08 <karthikeyanvishnu582@gmail.com>
2026-05-28 00:33:27 +0530

    mainlayout modified

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -4,23 +4,17 @@ import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 
 function MainLayout({ children }) {
-
   return (
-
     <div>
-
       <Navbar />
 
       <div className="layout-container">
-
         <Sidebar />
 
         <main className="layout-content">
           {children}
         </main>
-
       </div>
-
     </div>
   )
 }
